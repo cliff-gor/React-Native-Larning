58863328812a9a212c2bc94f9cf79065e65c2509
cliff-gor <cliff.gor.dev@gmail.com>
2025-01-18 11:41:11 +0300

    App icon change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! From Kodaschool</ThemedText>
+        <ThemedText type="title">Welcome! From Cliff Gor</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -68,3 +68,9 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
 });
+
+
+
+
+
+
